8f8c7c439482082c5d133371743e9cec8a61d883
mahsanaru <monemmahsa9@gmail.com>
2016-11-22 02:03:28 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
 <?php
-echo "<h1>Welcome to the best application in the world.";
-echo "<br><h1>Hello, world</h1>";
-echo "<br><h1>Hello, world</h1>";echo "<br><h1>Hello, world</h1>";echo "<br><h1>Hello, world</h1>";
+echo "<h1>Welcome to Openshift.";
+
